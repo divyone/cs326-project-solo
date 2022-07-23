758106fe6a340f60e38b8bd7056e077a7b84d575
Divyone Muigai <dmuigai@umass.edu>
2022-07-23 13:08:08 -0400

    Initialized npm and created express app

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import logger from 'morgan';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(logger('dev'));
+app.use('/', express.static('client'));
+
+
+
+
+// This matches all routes that are not defined.
+app.all('*', async (request, response) => {
+  response.status(404).send(`Not found: ${request.path}`);
+});
+
+app.listen(port, () => {
+  console.log(`Server started on poart ${port}`);
+});
